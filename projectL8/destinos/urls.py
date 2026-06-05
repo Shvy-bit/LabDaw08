b8903592b11691795d977f4fd1c8950587ad0b47
Shvy-bit <jcusiq@unsa.edu.pe>
2026-06-04 23:33:30 -0500

    Htmls en templates y ligera navegacion disponible

--- projectL8/destinos/urls.py
+++ projectL8/destinos/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
+    path('about', views.about, name = 'about'),
+    path('contact', views.contact, name = 'contact'),
+    path('destinations', views.destinations, name = 'destinations'),
+    path('news', views.news, name = 'news'),
+
 ]
--- projectL8/destinos/views.py
+++ projectL8/destinos/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def destinations(request):
+    return render(request, 'destinations.html')
+def news(request):
+    return render(request, 'news.html')
