648a66a9a648d913e67ce82d540303080bd76a16
Shvy-bit <jcusiq@unsa.edu.pe>
2026-06-05 01:30:53 -0500

    Modificacion de todos las plantillas para elementos estaticos

--- projectL8/destinos/views.py
+++ projectL8/destinos/views.py
@@ -4,13 +4,13 @@ from .models import DestinoTuristico
 # Create your views here.
 def index(request):
     destinos = DestinoTuristico.objects.all()
-
     return render(request, 'index.html', {'destinosHtml' : destinos})
 def about(request):
     return render(request, 'about.html')
 def contact(request):
     return render(request, 'contact.html')
 def destinations(request):
-    return render(request, 'destinations.html')
+    destinos = DestinoTuristico.objects.all()
+    return render(request, 'destinations.html', {'destinosHtml' : destinos})
 def news(request):
     return render(request, 'news.html')
